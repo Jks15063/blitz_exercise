29fb687a60ae1657c95dbfb33aed95620b2642a5
jks15063 <jks15063@gmail.com>
2022-01-14 01:09:46 -0700

    API client mostly done

--- lib/blitz_exercise/api_clients/riot_api_client.ex
+++ lib/blitz_exercise/api_clients/riot_api_client.ex
@@ -1,8 +1,6 @@
 defmodule BlitzExercise.ApiClients.RiotApiClient do
   @behaviour BlitzExercise.RiotApiBehaviour
-
   @api_key Application.get_env(:blitz_exercise, :riot_api_key)
-
   @headers %{
     "Accept-Language" => "en-US,en;q=0.5",
     "Accept-Charset" => "application/x-www-form-urlencoded; charset=UTF-8",
@@ -10,53 +8,47 @@ defmodule BlitzExercise.ApiClients.RiotApiClient do
   }
 
   @impl BlitzExercise.RiotApiBehaviour
-  def fetch_summoner(summoner_name, region \\ "na1") do
+  def fetch_summoner_puuid(summoner_name, region \\ "na1") do
     url = "https://#{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/#{summoner_name}"
 
     url
-    |> HTTPoison.get(@headers, params: %{api_key: @api_key})
-    |> handle_resp()
+    |> HTTPoison.get!(@headers, params: %{api_key: @api_key})
+    |> Map.get(:body)
+    |> Jason.decode!()
+    |> Map.get("puuid")
   end
 
-  def fetch_match_list(puuid, region \\ "americas") do
+  def fetch_champion_played_from_match(match_id, puuid, region \\ "americas") do
+    match_id
+    |> fetch_match_details(region)
+    |> summoner_champion_name(puuid)
+  end
+
+  def fetch_last_five_champions_played(puuid, region \\ "americas") do
+    puuid
+    |> fetch_match_list(region)
+    |> Enum.map(&fetch_match_details(&1, region))
+    |> Enum.map(&summoner_champion_name(&1, puuid))
+  end
+
+  defp fetch_match_list(puuid, region) do
     url = "https://#{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/#{puuid}/ids"
     params = %{start: 0, count: 5, api_key: @api_key}
 
-    url
-    |> HTTPoison.get(@headers, params: params)
-    |> handle_resp()
+    resp = HTTPoison.get!(url, @headers, params: params)
+    Jason.decode!(resp.body)
   end
 
-  def fetch_match_details(match_id, region \\ "americas") do
+  defp fetch_match_details(match_id, region) do
     url = "https://#{region}.api.riotgames.com/lol/match/v5/matches/#{match_id}"
 
-    url
-    |> HTTPoison.get(@headers, params: %{api_key: @api_key})
-    |> handle_resp()
+    resp = HTTPoison.get!(url, @headers, params: %{api_key: @api_key})
+    Jason.decode!(resp.body)
   end
 
-  def fetch_summoner_champion_from_match(match_id, summoner_id, region \\ "americas") do
-    match_id
-    |> fetch_match_details()
-    |> summoner_champion_name(summoner_id)
-  end
-
-  defp summoner_champion_name({:ok, %{"info" => %{"participants" => participants}}}, summoner_id) do
+  defp summoner_champion_name(%{"info" => %{"participants" => participants}}, puuid) do
     participants
-    |> Enum.find(fn participant -> participant["puuid"] == summoner_id end)
+    |> Enum.find(fn participant -> participant["puuid"] == puuid end)
     |> Map.fetch!("championName")
-  end
-
-  defp handle_resp(resp) do
-    case resp do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        {:ok, Jason.decode!(body)}
-
-      {:ok, %HTTPoison.Response{body: body}} ->
-        {:error, Jason.decode!(body)}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
-    end
   end
 end
